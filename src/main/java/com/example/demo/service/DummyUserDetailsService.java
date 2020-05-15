f165ac5d6115fc919f0387cc5c6842d0c57a3fef
Nguyen Xuan Linh <linh.nguyen@playnext-lab.co.jp>
2020-05-15 23:11:03 +0900

    first commit

---
+++ src/main/java/com/example/demo/service/DummyUserDetailsService.java
@@ -0,0 +1,15 @@
+package com.example.demo.service;
+
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public class DummyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username){
+		return new User(username, "notUsed", true, true, true, true, AuthorityUtils.createAuthorityList("ROLE_USER"));
+	}
+
+}
